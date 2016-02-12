1721a59dd479f23db06d2a2ba5ea5beff368cf8b
Devonte M <devontem@live.com>
2016-02-11 19:49:00 -0800

    finished gruntfile.js

--- app/models/user.js
+++ app/models/user.js
@@ -2,30 +2,7 @@
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
-// var environment = process.env.NODE_ENV || 'development';
 
-  
-  // var User = db.Model.extend({
-  //   tableName: 'users',
-  //   hasTimestamps: true,
-  //   initialize: function(){
-  //     this.on('creating', this.hashPassword);
-  //   },
-  //   comparePassword: function(attemptedPassword, callback) {
-  //     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-  //       callback(isMatch);
-  //     });
-  //   },
-  //   hashPassword: function(){
-  //     var cipher = Promise.promisify(bcrypt.hash);
-  //     return cipher(this.get('password'), null, null).bind(this)
-  //       .then(function(hash) {
-  //         this.set('password', hash);
-  //       });
-  //   }
-  // });
-
-  //add functions
   var Schema = mongoose.Schema;
   var userSchema = new Schema({
     username : {
@@ -44,30 +21,22 @@ var mongoose = require('mongoose');
   });
 
 
-  
+  var User = mongoose.model('User', userSchema);
 
-  userSchema.methods.comparePassword = function(attemptedPassword, savedPassword, callback) {
+  User.comparePassword = function(attemptedPassword, savedPassword, callback) {
     bcrypt.compare(attemptedPassword, savedPassword, function(err, isMatch) {
       callback(isMatch);
       });
   }
 
-  userSchema.methods.hashPassword = function() {
+  userSchema.pre('save', function(next) {
     var cipher = Promise.promisify(bcrypt.hash);
       return cipher(this.password, null, null).bind(this)
         .then(function(hash) {
           this.password = hash;
+          next();
         });
-  }
-
-  userSchema.pre('save', function(next) {
-    process.stdout.stream('hashpassword', this);
-    console.log('hashpassword', this);
-    this.hashPassword();
-    next();
-  })
-
-  var User = mongoose.model('User', userSchema);
+  });
 
 module.exports = User;
 
